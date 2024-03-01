c2755969de87e68512fdd4978923b0278db40a42
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-03-01 11:11:06 +0400

    refactor input system

--- 1-pong/Game.cs
+++ 1-pong/Game.cs
@@ -1,25 +1,39 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Game : Node2D
 {
 	private float playerSpeed = 300f;
 	private float paddleOffset = 40f;
 
+	private Dictionary<string, Tuple<PlayerKey, PaddleDirection>> paddleInputs = new Dictionary<string, Tuple<PlayerKey, PaddleDirection>>
+	{
+			{ "left_player_up", new Tuple<PlayerKey, PaddleDirection>(PlayerKey.Left, PaddleDirection.Up) },
+			{ "left_player_down", new Tuple<PlayerKey, PaddleDirection>(PlayerKey.Left, PaddleDirection.Down) },
+			{ "right_player_up", new Tuple<PlayerKey, PaddleDirection>(PlayerKey.Right, PaddleDirection.Up) },
+			{ "right_player_down", new Tuple<PlayerKey, PaddleDirection>(PlayerKey.Right, PaddleDirection.Down) },
+	};
+
 	private GameBounds gameBounds;
-	private Player leftPlayer;
-	private Player rightPlayer;
+
+	private Dictionary<PlayerKey, Player> players = new Dictionary<PlayerKey, Player>();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		gameBounds = GetNode<GameBounds>("GameBounds");
 		var gameBoundsCenter = gameBounds.shape.Size.Y / 2;
-		leftPlayer = GetNode<Player>("LeftPlayer");
+		var leftPlayer = GetNode<Player>("LeftPlayer");
 		leftPlayer.GlobalPosition = new Vector2(paddleOffset, gameBoundsCenter);
 
-		rightPlayer = GetNode<Player>("RightPlayer");
+		var rightPlayer = GetNode<Player>("RightPlayer");
 		rightPlayer.GlobalPosition = new Vector2(gameBounds.shape.Size.X - paddleOffset, gameBoundsCenter);
+
+		players = new Dictionary<PlayerKey, Player>() {
+			{ PlayerKey.Left, leftPlayer },
+			{ PlayerKey.Right, rightPlayer }
+		};
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,43 +43,50 @@ public partial class Game : Node2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		var leftPlayerDirection = 0;
-
-		if (Input.IsActionPressed("left_player_up"))
+		foreach (var entry in paddleInputs)
 		{
-			leftPlayerDirection = -1;
-
+			if (Input.IsActionPressed(entry.Key))
+			{
+				handlePaddleInput(entry.Value.Item1, entry.Value.Item2, delta);
+			}
 		}
-		else if (Input.IsActionPressed("left_player_down"))
-		{
-			leftPlayerDirection = 1;
-		}
-		handlePlayerInput(leftPlayer, leftPlayerDirection, delta);
-
-		var rightPlayerDirection = 0;
-
-		if (Input.IsActionPressed("right_player_up"))
-		{
-			rightPlayerDirection = -1;
-
-		}
-		else if (Input.IsActionPressed("right_player_down"))
-		{
-			rightPlayerDirection = 1;
-		}
-		handlePlayerInput(rightPlayer, rightPlayerDirection, delta);
 	}
 
-	private void handlePlayerInput(Player player, int direction, double delta)
+	private void handlePaddleInput(PlayerKey key, PaddleDirection direction, double delta)
 	{
-		if (direction != 0)
+		var directionSign = 0;
+
+		if (direction == PaddleDirection.Up)
 		{
+			directionSign = -1;
+
+		}
+		else if (direction == PaddleDirection.Down)
+		{
+			directionSign = 1;
+		}
+
+		if (directionSign != 0)
+		{
+			var player = players[key];
 			var currentPosition = player.GlobalPosition;
 			var newPosition = new Vector2(
 				currentPosition.X,
-				currentPosition.Y + direction * playerSpeed * (float)delta
+				currentPosition.Y + directionSign * playerSpeed * (float)delta
 			);
 			player.GlobalPosition = newPosition;
 		}
+	}
+
+	enum PlayerKey
+	{
+		Left,
+		Right
+	}
+
+	enum PaddleDirection
+	{
+		Up,
+		Down
 	}
 }
