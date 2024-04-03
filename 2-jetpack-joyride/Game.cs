385aed571ef71cf5d32bbd09dc5ba8fb8d378679
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-04-03 23:52:47 +0400

    add game bounds

--- 2-jetpack-joyride/Game.cs
+++ 2-jetpack-joyride/Game.cs
@@ -3,14 +3,29 @@ using System;
 
 public partial class Game : Node2D
 {
+
+	private GameBounds gameBounds;
+	private Player player;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("Hello World");
+		gameBounds = GetNode<GameBounds>("GameBounds");
+		player = GetNode<Player>("Player");
+
+		InitGame();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void InitGame()
+	{
+		GD.Print($"{player.GlobalPosition} {gameBounds.GlobalPosition}");
+		player.GlobalPosition = gameBounds.GlobalPosition;
+		GD.Print($"{player.GlobalPosition} {gameBounds.GlobalPosition}");
 	}
 }
