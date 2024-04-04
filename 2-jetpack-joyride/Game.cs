5f9d3ce7856e4f4064ef24270205e0910c59338d
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-04-04 11:16:46 +0400

    add one obstacle

--- 2-jetpack-joyride/Game.cs
+++ 2-jetpack-joyride/Game.cs
@@ -1,11 +1,14 @@
 using Godot;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public partial class Game : Node2D
 {
 
 	private GameBounds gameBounds;
 	private Player player;
+	private List<Obstacle> obstacles = new List<Obstacle>();
 
 	private Vector2 playerVelocity;
 	private bool isGrounded = false;
@@ -16,6 +19,7 @@ public partial class Game : Node2D
 		GD.Print("Hello World");
 		gameBounds = GetNode<GameBounds>("GameBounds");
 		player = GetNode<Player>("Player");
+		obstacles.Add(GetNode<Obstacle>("Obstacle"));
 
 		InitGame();
 	}
@@ -54,15 +58,36 @@ public partial class Game : Node2D
 			playerVelocity += new Vector2(0f, gravityAcceleration) * (float)delta;
 		}
 		GD.Print($"player velocity: {playerVelocity} player position: {player.GlobalPosition}");
+
+		obstacles.ForEach((obstacle) =>
+		{
+			obstacle.GlobalPosition = new Vector2(
+									obstacle.GlobalPosition.X - obstacleSpeed * (float)delta,
+									obstacle.GlobalPosition.Y
+								);
+			// if (obstacle.GlobalPosition.X > gameBounds.GlobalPosition.X + gameBounds.shape.Size.X)
+			// {
+			// 	obstacle.GlobalPosition = new Vector2(
+			// 						obstacle.GlobalPosition.X - obstacleSpeed * (float)delta,
+			// 						obstacle.GlobalPosition.Y
+			// 					);
+			// }
+			GD.Print($"obstacle: {obstacle.GlobalPosition}");
+		});
 	}
 
 	private void InitGame()
 	{
 		playerVelocity = Vector2.Zero;
 		player.GlobalPosition = gameBounds.GlobalPosition;
+		obstacles.ForEach((obstacle) =>
+		{
+			obstacle.GlobalPosition = new Vector2(gameBounds.GlobalPosition.X + gameBounds.shape.Size.X + 100, gameBounds.GlobalPosition.Y);
+		});
 	}
 
 	private float gravityAcceleration = 9.8f * 20;
 	private float jetpackForce = 750f;
+	private float obstacleSpeed = 200f;
 
 }
