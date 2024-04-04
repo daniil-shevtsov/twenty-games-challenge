f036e53b5c058f6e15777097d98ac3122e7bd649
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-04-04 11:24:55 +0400

    add changing obstacle size

--- 2-jetpack-joyride/Game.cs
+++ 2-jetpack-joyride/Game.cs
@@ -82,12 +82,13 @@ public partial class Game : Node2D
 		player.GlobalPosition = gameBounds.GlobalPosition;
 		obstacles.ForEach((obstacle) =>
 		{
+			obstacle.SetSize(new Vector2(100f, 50f));
 			obstacle.GlobalPosition = new Vector2(gameBounds.GlobalPosition.X + gameBounds.shape.Size.X + 100, gameBounds.GlobalPosition.Y);
 		});
 	}
 
 	private float gravityAcceleration = 9.8f * 20;
 	private float jetpackForce = 750f;
-	private float obstacleSpeed = 200f;
+	private float obstacleSpeed = 400f;
 
 }
---
+++ 2-jetpack-joyride/Obstacle.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Obstacle : StaticBody2D
+{
+	public CollisionShape2D collisionShape;
+	public RectangleShape2D shape;
+	private Polygon2D sprite;
+	private Vector2 obstacleSize;
+
+	public void SetSize(Vector2 size)
+	{
+		obstacleSize = size;
+		shape.Size = obstacleSize;
+		var newPoints = new List<Vector2>
+		{
+			new(-obstacleSize.X / 2, -obstacleSize.Y / 2),
+			new(obstacleSize.X / 2, -obstacleSize.Y / 2),
+			new(obstacleSize.X / 2, obstacleSize.Y / 2),
+			new(-obstacleSize.X / 2, obstacleSize.Y / 2),
+		};
+		sprite.Polygon = newPoints.ToArray();
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+		shape = (RectangleShape2D)collisionShape.Shape;
+		sprite = GetNode<Polygon2D>("Polygon2D");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
