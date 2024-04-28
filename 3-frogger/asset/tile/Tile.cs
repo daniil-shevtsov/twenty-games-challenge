f61346b9c1869bc9626b2af7609d7c2c02d42e18
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-04-29 01:00:44 +0400

    create tile

--- 3-frogger/asset/tile/Tile.cs
+++ 3-frogger/asset/tile/Tile.cs
@@ -1,11 +1,20 @@
 using Godot;
 using System;
+using System.Drawing;
 
-public partial class Tile : Node2D
+public partial class Tile : StaticBody2D
 {
+	CollisionShape2D collisionShape;
+	RectangleShape2D shape;
+	ColorRect background;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+		shape = (RectangleShape2D)collisionShape.Shape;
+		background = GetNode<ColorRect>("ColorRect");
+
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
